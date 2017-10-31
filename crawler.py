663daf584d3750cce63f9aec4ee5e3c076ce423d
Joy <piaozai88@163.com>
2017-10-31 14:11:27 +0800

    bs4 for web crawler

---
+++ crawler.py
@@ -0,0 +1,13 @@
+import urllib2
+from bs4 import BeautifulSoup
+
+def search_links(symbol):
+    url = 'https://seekingalpha.com/symbol/'+ symbol + '/focus'
+    hdr = {'User-Agent': 'Mozilla/5.0'}
+    req = urllib2.Request(url, headers=hdr)
+    html = urllib2.urlopen(req).read()
+    soup = BeautifulSoup(html)
+    return soup
+
+tables = soup.find_all("div", class_="content_block_investment_views")[0]
+
